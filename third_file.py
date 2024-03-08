0c5222ac2728e032c65065cfc3d55e893d5ebf7b
vinay polineni <vinaypolineni98@gmail.com>
2024-03-08 23:34:00 +0530

    changed file name

--- third_file.py
+++ third_file.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    print("Rohit")
+    print("Polineni")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
