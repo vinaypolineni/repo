9bffde74e00e0aec923d304b4510d8564ba5d6a3
vinay polineni <vinaypolineni98@gmail.com>
2024-03-08 20:22:30 +0530

    chnaged code

--- second_file.py
+++ second_file.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    print("vinay")
+    print("polineni")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
