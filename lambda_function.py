96ceee6d6198248aeb1a18d370e33c6508207421
vinay polineni <vinaypolineni98@gmail.com>
2024-03-08 14:05:28 +0530

    upatedlambda

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,7 @@ def lambda_handler(event, context):
     # TODO implement
     print("Hello world")
     print("event")
+    print("vinay")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
